5409297d34bb674d038d8e79fa01708acab214c5
diana pardy <dianapardy91@gmail.com>
2019-04-23 12:31:44 -0600

    updated files

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,4 +1,20 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WelcomeController {
+    private final String myMessage;
+
+    public WelcomeController(
+        @Value("${welcome.message:NOT SET}") String msg) {
+        this.myMessage = msg;
+    }
+
+    @GetMapping("/")
+    public String sayHello() {
+        return myMessage;
+    }
 }
